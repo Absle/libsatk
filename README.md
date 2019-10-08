51985b7fb663384f5ac453a43f6a6f9b77b49afa
Anderson Kagel <anderson.kagel@gmail.com>
2019-10-08 18:46:30 -0400

    added gitignore, added TODO to README, created project rc

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# libsatk
+libsatk
+=======
+
 My personal gamedev library. Mostly for experimenting with ideas and project design, but maybe one day an actual game will be made!
+
+TODO: this could really do with some expansion!
---
+++ tools/libsatkrc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# env variables
+export PROOT="/home/anderson/devel/libsatk"
+export PNAME="libsatk"
+export PBUILD_DIR="$PROOT/build"
+export PBUILD_MODE="Debug" # must be either "Debug" or "Release"
+
+# useful shortcuts
+alias root='cd $PROOT'
+alias build='cd $PBUILD_DIR/$PBUILD_MODE && cmake "$PROOT" && make'
+alias cdbuild='cd $PBUILD_DIR/$PBUILD_MODE'
+alias debug='export PBUILD_MODE="Debug" && build'
+alias release='export PBUILD_MODE="Release" && build'
+alias tools='cd $PROOT/tools'
+
+# reporting
+echo
+echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+echo "* Project:    $PNAME"
+echo "* Directory:  $PROOT"
+echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+echo
